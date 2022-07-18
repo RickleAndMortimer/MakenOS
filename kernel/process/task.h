bd34daaf3dd76a26f6b79864cd8537849f19488e
RickleAndMortimer <nathacutlan@gmail.com>
2022-07-18 00:13:45 -0400

    task structs

---
+++ kernel/process/task.h
@@ -0,0 +1,50 @@
+#include <stdint.h>
+
+typedef struct Task 
+{
+	uint64_t cs, ds, fs, gs; 
+	// General purpose registers
+	uint64_t rax, rbx, rcx, rdx, rsi, rdi, rsp, rbp;
+	uint64_t r8, r9, r10, r11, r12, r13, r14, r15;
+	uint64_t rflags; 
+	uint64_t cr3;
+	uint64_t state;
+	uint64_t ldtr;
+	uint64_t io_map_address, io_map;
+	// Stack pointers
+	uint64_t sp0, sp1, sp2;
+	Task* prev;
+	uint64_t ssp_state;
+} Task;
+
+typedef struct TSSDescriptor {
+	uint16_t limit1;
+	uint16_t base1;
+	uint8_t base2;
+	// type[4] 0 DPL[0:1] P[1]
+	uint8_t flags1;
+	// limit[16:19] AVL[1] 0 0 G[1]
+	uint8_t flags2;
+	uint8_t base3;
+	uint32_t base4;
+	uint32_t reserved;
+} TSSDescriptor
+
+typedef struct TSS 
+{
+	uint32_t reserved;	
+	uint64_t rsp0;
+	uint64_t rsp1;
+	uint64_t rsp2;
+	uint64_t reserved2;
+	uint64_t ist1;
+	uint64_t ist2;
+	uint64_t ist3;
+	uint64_t ist4;
+	uint64_t ist5;
+	uint64_t ist6;
+	uint64_t ist7;
+	uint64_t reserved3;	
+	uint16_t reserved4;	
+	uint16_t io_map_address;	
+}__attribute__((packed)) TSS;
